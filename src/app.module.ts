5a611d6ea1ee96c5e8b0f302b1b14e78699650d4
José Augusto <joseaugusto0408@gmail.com>
2022-12-13 11:27:35 -0300

    prisma e db notification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from './http.module';
+import { MailService } from './mail/mail.service';
+import { PostmarkMailService } from './mail/postmark-mail.service';
+import { SMTPMailService } from './mail/smtp-mail.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: MailService,
+      useClass: PostmarkMailService,
+    },
+  ],
 })
 export class AppModule {}
